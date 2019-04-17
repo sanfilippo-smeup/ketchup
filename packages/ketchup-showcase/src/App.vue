f7e2c82c1f2e2d32bcebe3f48d470e43d4ec2e29
scrrnd <armando.scarpa@smeup.com>
2019-04-17 12:03:24 +0200

    initial creation

--- packages/ketchup-showcase/src/App.vue
+++ packages/ketchup-showcase/src/App.vue
@@ -2,17 +2,106 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <ketchup-fld id="fld1" first="Runaways" last="'Call me any time"></ketchup-fld>
+    <hr>
     <ketchup-button label="Sono bottone"></ketchup-button>
+    <hr>
     <ketchup-radio id="krg1"></ketchup-radio>
+    <hr>
     <ketchup-text-input id="int1" is-clearable initial-value="ciao"></ketchup-text-input>
+    <hr>
     <ketchup-combo initial-value="sono di prova" is-clearable></ketchup-combo>
+    <hr>
+    <MAT :content="matrixData">CIAO</MAT>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  components: {}
+  components: {},
+  matrixData: {
+                  "type": "EXB",
+                  "key": "i123",
+                  "title": "la mia matice ",
+                  "options": {
+                    "EXB": {
+                      "": {
+                        "enableSort": true
+                      },
+                      "A01": {
+                        "showFilter": true
+                      }
+                    },
+                    "CHA": {
+                      "": {
+                        "type": "BAR"
+                      }
+                    }
+                  },
+                  "data": {
+                    "columns": [
+                      {
+                        "name": "nome della colonna -> code",
+                        "title": "titolo -> text",
+                        "size": ""
+                      },
+                      {
+                        "name": "FLD1",
+                        "title": "column 2",
+                        "size": 10
+                      }
+                    ],
+                    "rows": [
+                      {
+                        "object": "se presente, il K01",
+                        "readOnly": true,
+                        "cells": {
+                          "FLD1": {
+                            "readOnly": true,
+                            "options (sarebbe il tasto destro)": true,
+                            "obj": {
+                              "t": "CN",
+                              "p": "COL",
+                              "k": "KEKBUR"
+                            },
+                            "style": "",
+                            "type": "itx / acp / etc...",
+                            "value": "mickey mouse"
+                          },
+                          "FLD2": {
+                            "value": "value 2"
+                          }
+                        }
+                      }
+                    ]
+                  },
+                  "messages": [],
+                  "actions": {
+                    "row": [
+                      {
+                        "exec": "...",
+                        "icon": "mdi-clock",
+                        "text": "Scheda orologio"
+                      }
+                    ],
+                    "global": [],
+                    "auto (tag action attuale)": [
+                      "F() / CLOSE / ETC...",
+                      "F() / CLOSE / ETC...",
+                      "F() / CLOSE / ETC...",
+                      "F() / CLOSE / ETC..."
+                    ],
+                    "command": [
+                      {
+                        "exec": "...",
+                        "icon": "mdi-play",
+                        "text": "Spotify"
+                      }
+                    ]
+                  },
+                  "variables": [],
+                  "dynamisms": []
+                }
 }
 </script>
 
--- packages/ketchup-showcase/src/main.js
+++ packages/ketchup-showcase/src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue';
 import { defineCustomElements } from 'ketchup/dist/loader';
 defineCustomElements(window);
 
+//import Matrix from '../../ketchup-vue/src/components/smeup/MAT.vue'
+//Vue.component('MAT', Matrix)
+//import KetchupVue from "ketchup-vue";
+//Vue.use(KetchupVue);
+
 Vue.config.productionTip = false;
 Vue.config.ignoredElements = [/[ketchup|app]-\w*/];
 
